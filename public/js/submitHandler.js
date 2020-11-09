477d6e2d7165a117d2b5f5244a22e1fdc8a43c9a
Lance Avitia <lance77a@gmail.com>
2020-11-08 21:48:55 -0800

    Layout for /events finished. Moving to styling and finishing touches.

---
+++ public/js/submitHandler.js
@@ -0,0 +1,2 @@
+const message = "You've successfully Submitted! If this was your site, you'd be receiving the data in an email from a form handling API."
+$('#submit').on("click", () => alert(message));
